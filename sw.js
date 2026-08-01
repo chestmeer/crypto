cb0d06a8eeeefbcb1dc00b139a779ac8db2d6e86
chestmeer <xyz159369@gmail.com>
2026-08-01 23:51:49 +0200

    commit8

--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
-self.addEventListener('fetch', (event) => {});
+// KOD DLA OPCJI B (Rekomendowany dla aplikacji mobilnych)
+const CACHE_NAME = 'moja-aplikacja-v1';
+
+// Pliki, które mają działać bez internetu (zmień nazwy na swoje!)
+const ASSETS = [
+  './',
+  './icon192.png',
+  './icon512.png',
+  './index.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
